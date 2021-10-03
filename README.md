c6634dec1b7dd0b94eb66a9dfedcda0a6f8ef5da
Evgeny Varganov <gokjer@gmail.com>
2021-10-04 01:02:07 +0300

    Documentation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # csc-db-test
+
+For usage please refer to the code or 
+```
+python table_generator.py --help
+```
+Example of usage:
+```bash
+python table_generator.py --n_rows=20 --out-file='out.csv' --columns="name:str, age :  int , suprize" --conditions="name=Johnny, age" --condition_frequency=0.1 --guaranteed=true
+```
--- table_generator.py
+++ table_generator.py
@@ -83,8 +83,27 @@ def generate_table(n_rows, out_file, out_file_correct, column_list, column_types
             main_writer.writerow(row)
 
 
-def main(n_rows, out_file, out_file_correct=None, columns=None, n_columns=None, seed=None, conditions=None,
-         condition_frequency=0.1, guaranteed=False):
+def main(n_rows: int, out_file: str, out_file_correct: str = None, columns: str = None, n_columns: int = None,
+         seed: int = None, conditions: str = None, condition_frequency: float = 0.1, guaranteed: bool = False):
+    """
+    Generate a random csv file with given parameters
+    :param n_rows: number of rows in the resulting table
+    :param out_file: path to save main table
+    :param out_file_correct: Path to save table with correct rows. Generated based on out_file if not given.
+    :param columns: Comma-separated list of columns. A type may be specified for each column, for example:
+    --columns="id: int,  field_with_random_type". If not specified, chosen on random.
+    Whitespace-insensitive. Available types: int, str, hex.
+    "any" type is also valid, but works same as no specified type.
+    :param n_columns: Number of columns. If greater than provided in $columns, add random columns "column_{i}".
+    :param seed: Random seed. random.seed() with no argument is called if not provided.
+    :param conditions: Comma-separated list of conditions "column_name[=value]". Whitespace-insensitive.
+    If value is not specified, it is generated randomly (but is still unique).
+    Example: --conditions="height=200, weight = 80, name".
+    :param condition_frequency: Frequency of correct rows. Ranges in [0.0, 1.0).
+    :param guaranteed: Whether we want there to be at least one correct row
+    (forces the last row to be correct if none were correct before).
+    :return:
+    """
     if columns is None and n_columns is None:
         raise ValueError('Must specify either columns or n_columns argument')
     if seed is None:
